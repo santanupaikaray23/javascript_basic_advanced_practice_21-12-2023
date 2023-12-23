21ea26d43bd271cf7a062288112340190646836b
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2023-12-23 16:43:06 +0530

    add all file changes

---
+++ loop.js
@@ -0,0 +1,28 @@
+for
+while
+do while
+for of
+for in
+Map
+filter
+
+///////////
+for> generate series of value or help to iterate over Array
+
+for (i=0;i<5;i++){
+    console.log(i)
+}
+
+var city = ["London","Delhi","NewYork","Paris"]
+for (i=0;i<city.length;i++){
+    console.log(city[i])
+}
+
+var city = ["London","Delhi","NewYork","Paris"]
+for (i=0;i<city.length;i++){
+    console.log(city[i])
+}
+ London
+ Delhi
+ NewYork
+ Paris
